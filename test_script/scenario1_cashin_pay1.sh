bb216f1d27f2c12208872e65f37d9ae4b18a44e5
SuperKid2205 <SuperKid2205@gmail.com>
2025-02-06 10:25:05 +0700

    add cashin

--- test_script/scenario1_cashin_pay1.sh
+++ test_script/scenario1_cashin_pay1.sh
@@ -4,6 +4,10 @@ cd ..
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/target/classes"
 export CLASSPATH=$DIR
 
+java org.momo.Main CASH_IN 1000
+echo -----------------
+java org.momo.Main CASH_IN 1000
+echo -----------------
 java org.momo.Main LIST_BILL
 echo -----------------
 java org.momo.Main PAY 2
